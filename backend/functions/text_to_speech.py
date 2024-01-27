5ab22fae3be3f5d310073dbf9ecf846153a67914
gadnadj <gadnadjar@gmail.com>
2024-01-27 18:00:29 +0200

    add more voices

--- backend/functions/text_to_speech.py
+++ backend/functions/text_to_speech.py
@@ -10,8 +10,8 @@ def convert_text_to_speech(message, selected_voice):
     # Map the voice name to the corresponding ID
     voice_mapping = {
         "shaun": "9F4C8ztpNUmXkdDDbz3J",
-        "Jarvis": "21m00Tcm4TlvDq8ikWAM",
-        "antoni": "9F4C8ztpNUmXkdDDbz3J",
+        "Jarvis": "zr8KTOWwGVdfwNMcTtgs",
+        "antoni": "DZyyFMXNpavMudLSm7A9",
         "sarah": "yi1Q6GP2jWKJk3kJ1uIS"
         # Add more voices as needed
     }
